d2d69c2faf3c6403a34d273d6bcb977bed9873e1
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-18 09:30:00 +0700

    Update main.cpp

--- Fnoberz C++/main.cpp
+++ Fnoberz C++/main.cpp
@@ -98,7 +98,6 @@ bool CreateDeviceD3D(HWND hWnd)
 
     if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
         return false;
-    return false;
 }
 
 void CleanupDeviceD3D()
@@ -179,12 +178,14 @@ void Spoofer::GetFiveM() {
 		delfiles += std::filesystem::remove_all(asifive);
 		std::cout << "\x1B[31m[\033[0m\x1B[33m!\033[0m\x1B[31m]\033[0m Removing FiveM game files... (to initialize update) " << std::endl;
 		std::cout << "\x1B[31m[\033[0m\x1B[32m!\033[0m\x1B[31m]\033[0m Deleted \x1B[96mFiveM\033[0m " << delfiles << " files or directories\n";
+		
+		auto* buffer = static_cast<char*>(ExAllocatePoolWithTag(NonPagedPool, length, POOL_TAG));
+			buffer[length] = '\0';
+		
+		
 		if (delfiles <= 0) {
 			std::cout << "\x1B[31m[\033[0m\x1B[91m!\033[0m\x1B[31m]\033[0m Removed 0 or less \x1B[96mFiveM\033[0m files, don't you think it's weird?" << std::endl;
 		}
-	}
-	ImGui::BeginChild(id.c_str(), ImVec2(width, height), visible); 
-	ImGui::EndChild();
 
 	std::cout << "\x1B[31m[\033[0m\x1B[91m!\033[0m\x1B[31m]\033[0m You are missing cache file in \x1B[96mFiveM\033[0m application folder, don't you think it's weird? | SKIPPING" << std::endl;
 	}
