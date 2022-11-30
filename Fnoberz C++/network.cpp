d9f7e1cbed5ce1c916da6dd58625c3eb55e3bf16
Fnoberz <SarnaxOfficial@protonmail.com>
2022-12-01 00:03:39 +0700

    Update network.cpp

--- Fnoberz C++/network.cpp
+++ Fnoberz C++/network.cpp
@@ -82,23 +82,14 @@ void network::destroy()
 	}
 }
 
-void network::Shutdown()
+
+void GetHWID()
+
 {
-	// BLock IP address Process
-	auto ip_block = g_trace->m_fivem_path + "\\FiveM.app\\data\\cache\\subprocess\\FiveM_GTAProcess.exe";
 	
-	delete_file(ip_block);
-	block_connection(ip_block);
-	
-	auto Shutdown("FiveM_GTAProcess.exe"");
 }
+    get a handle to the first physical drive
 
-}
-
-
-std::string GetHWID()
-{
-    //get a handle to the first physical drive
     HANDLE h = CreateFileW(L"\\\\.\\PhysicalDrive0", 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
     if (h == INVALID_HANDLE_VALUE) return {};
 
