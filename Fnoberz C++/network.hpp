317c5eefc3671bb61178c5631e4a2c14e1479cbb
Developer <81179408+RutOf@users.noreply.github.com>
2022-12-16 23:54:28 +0700

    Update network.hpp

--- Fnoberz C++/network.hpp
+++ Fnoberz C++/network.hpp
@@ -4,18 +4,31 @@
 class network
 {
 public:
-	void setup();
-	void destroy();
+    void setup();
+    void destroy();
 private:
-	void block_connection(std::string process);
-	void unblock_connection(std::string process);
+    void block_connection(std::string process);
+    void unblock_connection(std::string process);
 };
 
 inline std::unique_ptr<network> g_network;
 
-	if
-	{
-		network::bypass 
-		{
-			{
-				
+void network::setup()
+{
+    // Add code here to set up the network
+}
+
+void network::destroy()
+{
+    // Add code here to clean up and destroy the network
+}
+
+void network::block_connection(std::string process)
+{
+    // Add code here to block a connection for the specified process
+}
+
+void network::unblock_connection(std::string process)
+{
+    // Add code here to unblock a connection for the specified process
+}
