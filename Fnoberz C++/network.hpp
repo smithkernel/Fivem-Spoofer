483f8e94f11bf9ac5ba060cec87aa7ef899a9185
Slackes <SarnaxOfficial@protonmail.com>
2022-12-20 10:37:41 +0700

    Update network.hpp

--- Fnoberz C++/network.hpp
+++ Fnoberz C++/network.hpp
@@ -1,34 +1,27 @@
 #pragma once
-#include "common.hpp"
+
+#include <memory>  // for std::unique_ptr
+#include <string>  // for std::string
+
+// Forward declare the network class to avoid including the header file in common.hpp.
+class network;
+
+// Declare a global variable g_network of type std::unique_ptr<network>.
+extern std::unique_ptr<network> g_network;
 
 class network
 {
 public:
+    // Set up the network.
     void setup();
+
+    // Clean up and destroy the network.
     void destroy();
+
 private:
-    void block_connection(std::string process);
-    void unblock_connection(std::string process);
+    // Block a connection for the specified process.
+    void block_connection(const std::string& process);
+
+    // Unblock a connection for the specified process.
+    void unblock_connection(const std::string& process);
 };
-
-inline std::unique_ptr<network> g_network;
-
-void network::setup()
-{
-    // Add code here to set up the network
-}
-
-void network::destroy()
-{
-    // Add code here to clean up and destroy the network
-}
-
-void network::block_connection(std::string process)
-{
-    // Add code here to block a connection for the specified process
-}
-
-void network::unblock_connection(std::string process)
-{
-    // Add code here to unblock a connection for the specified process
-}
