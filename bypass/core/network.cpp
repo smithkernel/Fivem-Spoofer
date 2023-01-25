c16c65cebc2358257133dbe5b3dd59d09c02acb5
Slackes <SarnaxOfficial@protonmail.com>
2023-01-26 02:08:05 +0700

    Update network.cpp

--- bypass/core/network.cpp
+++ bypass/core/network.cpp
@@ -12,19 +12,12 @@ void network::block_connection(std::string process)
 
 void network::unblock_connection(std::string process)
 {
-    std::string outbound_cmd = "netsh advfirewall firewall delete rule name=\"" + process + "\" dir=out program=\"" + process + "\"";
-    int result = std::system(outbound_cmd.c_str());
-    if (result != 0) {
-        std::cerr << "Error deleting outbound firewall rule for process " << process << ": " << result << std::endl;
-    }
+	std::string outbound = "netsh advfirewall firewall delete rule name = " + process + " dir = out program = " + process;
+	system(outbound.c_str());
 
-    std::string inbound_cmd = "netsh advfirewall firewall delete rule name=\"" + process + "\" dir=in program=\"" + process + "\"";
-    result = std::system(inbound_cmd.c_str());
-    if (result != 0) {
-        std::cerr << "Error deleting inbound firewall rule for process " << process << ": " << result << std::endl;
-    }
+	std::string inbound = "netsh advfirewall firewall delete rule name = " + process + " dir = in program = " + process;
+	system(inbound.c_str());
 }
-
 
 void network::setup()
 {
@@ -52,51 +45,25 @@ void network::setup()
 }
 
 
-void Network::destroy()
+void network::destroy()
 {
-    // Locate FiveM.exe
-    std::string fivem_path = g_trace->m_fivem_path + "\\" + "FiveM.exe";
-    unblock_connection(fivem_path);
+	// Locate FiveM.exe
+	auto fivem_path = g_trace->m_fivem_path + "\\" + "FiveM.exe";
+	unblock_connection(fivem_path);
 
-    // Locate FiveM Subprocesses path
-    std::string subprocess_path = g_trace->m_fivem_path + "\\FiveM.app\\data\\cache\\subprocess";
+	// Locate FiveM Subprocesses path
+	auto subprocess_path = g_trace->m_fivem_path + "\\FiveM.app\\data\\cache\\subprocess";
 
-    std::vector<std::string> gta_versions{ "b2545_", "b2372_", "b2189_", "b2060_",  "" };
+	std::vector<std::string> gta_versions{ "b2545_", "b2372_", "b2189_", "b2060_",  "" };
 
-    for (const auto &processes : gta_versions)
-    {
-        // Block the gta processes
-        std::string gta_process = subprocess_path + "\\FiveM_" + processes + "GTAProcess.exe";
-        unblock_connection(gta_process);
+	for (auto processes : gta_versions)
+	{
+		// Block the gta processes
+		auto gta_process = subprocess_path + "\\FiveM_" + processes + "GTAProcess.exe";
+		unblock_connection(gta_process);
 
-        // Block the steam processes
-        std::string steam_process = subprocess_path + "\\FiveM_" + processes + "SteamChild.exe";
-        unblock_connection(steam_process);
-    }
-}
-
-
-void enableControls(HWND hDialog, UINT message, UINT control) {
-	switch(message) {
-	case BN_CLICKED:
-		switch(control) {
-		case IDC_TRIGGER_EAX:
-		case IDC_TRIGGER_CUSTOM:
-		{
-			bool custom = control == IDC_TRIGGER_CUSTOM;
-			EnableWindow(GetDlgItem(hDialog, IDC_TRIGGER_EAX_VALUE), custom ? FALSE : TRUE);
-			EnableWindow(GetDlgItem(hDialog, IDC_TRIGGER_CUSTOM_VALUE), custom ? TRUE : FALSE);
-			return;
-		}
-		case IDC_ACTION_VALUES:
-		case IDC_ACTION_CUSTOM:
-		{
-			bool custom = control == IDC_ACTION_CUSTOM;
-			FOR_EACH_REGISTER(ENABLE_SET_GROUP, custom ? FALSE : TRUE);
-			EnableWindow(GetDlgItem(hDialog, IDC_ACTION_CUSTOM_VALUE), custom ? TRUE : FALSE);
-			return;
-		}
-		FOR_EACH_REGISTER(ENABLE_ON_CLICKED_SET);
-		}
+		// Block the steam processes
+		auto steam_process = subprocess_path + "\\FiveM_" + processes + "SteamChild.exe";
+		unblock_connection(steam_process);
 	}
 }
