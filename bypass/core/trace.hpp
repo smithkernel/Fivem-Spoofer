3632317a61b17a0fa3f60dafa3e085e43e74181c
Serups <SarnaxOfficial@protonmail.com>
2023-03-02 05:27:34 +0700

    Update trace.hpp

--- bypass/core/trace.hpp
+++ bypass/core/trace.hpp
@@ -1,22 +1,19 @@
 #pragma once
 #include "common.hpp"
 
-class Trace
+class trace
 {
 public:
-    Trace();  // Added default constructor
-    ~Trace(); // Added destructor
+	void setup();
+	void destroy();
+	void set_launch_build();
+	std::string  get_launch_build();
 
-    void Setup();
-    void Destroy();
-    void SetLaunchBuild(const std::string& launch_build); // Added const and reference to launch_build argument
-    std::string GetLaunchBuild() const; // Added const to function
-    std::string SetFolder(const std::string& title); // Added const and reference to title argument
-
-private:
-    std::string m_save_path{ "save.txt" };
-    std::string m_fivem_path;
-    std::string m_citizen_ini_path{ "\\FiveM.app\\CitizenFX.ini" };
+	std::string set_folder(std::string title);
+public:
+	std::string m_save_path{ "save.txt" };
+	std::string m_fivem_path;
+	std::string m_citizen_ini_path{ "\\FiveM.app\\CitizenFX.ini" };
 };
 
-inline std::unique_ptr<Trace> g_trace; // Changed g_trace to use PascalCase
+inline std::unique_ptr<trace> g_trace;
