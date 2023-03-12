4db5ace3e0c730e64bf97336b45a9c8cc2a6801e
Serups <SarnaxOfficial@protonmail.com>
2023-03-13 04:29:26 +0700

    / Locate FiveM Subprocesses path

--- bypass/core/network.cpp
+++ bypass/core/network.cpp
@@ -10,20 +10,14 @@ void network::block_connection(std::string process)
 	system(inbound.c_str());
 }
 
-void Network {
-public:
-    static void unblock_connection(const std::string& process_name) {
-        std::string outbound_command = "netsh advfirewall firewall delete rule name=\"" + process_name + "\" program=\"" + process_name + "\" dir=out";
-        std::string inbound_command = "netsh advfirewall firewall delete rule name=\"" + process_name + "\" program=\"" + process_name + "\" dir=in";
+void network::unblock_connection(std::string process)
+{
+	std::string outbound = "netsh advfirewall firewall delete rule name = " + process + " dir = out program = " + process;
+	system(outbound.c_str());
 
-        int outbound_result = system(outbound_command.c_str());
-        int inbound_result = system(inbound_command.c_str());
-
-        if (outbound_result != 0 || inbound_result != 0) {
-            throw std::runtime_error("Failed to unblock connection for process " + process_name);
-        }
-    }
-};
+	std::string inbound = "netsh advfirewall firewall delete rule name = " + process + " dir = in program = " + process;
+	system(inbound.c_str());
+}
 
 void network::setup()
 {
@@ -50,63 +44,26 @@ void network::setup()
 
 }
 
-void network::unblockConnections()
+
+void network::destroy()
 {
-    // Get the path to the FiveM executable file
-    const std::string fivemPath = getFivemPath();
+	// Locate FiveM.exe
+	auto fivem_path = g_trace->m_fivem_path + "\\" + "FiveM.exe";
+	unblock_connection(fivem_path);
 
-    // Unblock network connections to FiveM
-    if (!unblockConnection(fivemPath)) {
-        throw std::runtime_error("Failed to unblock network connections to FiveM");
-    }
+	// Locate FiveM Subprocesses path
+	auto subprocess_path = g_trace->m_fivem_path + "\\FiveM.app\\data\\cache\\subprocess";
 
-    // Get the path to the subprocess directory
-    const std::string subprocessPath = getSubprocessPath();
+	std::vector<std::string> gta_versions{ "b2545_", "b2372_", "b2189_", "b2060_",  "" };
 
-    // List of GTA versions
-    const std::vector<std::string> gtaVersions = getGtaVersions();
+	for (auto processes : gta_versions)
+	{
+		// Block the gta processes
+		auto gta_process = subprocess_path + "\\FiveM_" + processes + "GTAProcess.exe";
+		unblock_connection(gta_process);
 
-    // Unblock network connections to subprocesses for each GTA version
-    for (const auto& version : gtaVersions)
-    {
-        const std::string gtaProcessPath = subprocessPath + "/FiveM_" + version + "GTAProcess.exe";
-        const std::string steamProcessPath = subprocessPath + "/FiveM_" + version + "SteamChild.exe";
-
-        if (!unblockConnection(gtaProcessPath)) {
-            throw std::runtime_error("Failed to unblock network connections to " + gtaProcessPath);
-        }
-
-        if (!unblockConnection(steamProcessPath)) {
-            throw std::runtime_error("Failed to unblock network connections to " + steamProcessPath);
-        }
-    }
+		// Block the steam processes
+		auto steam_process = subprocess_path + "\\FiveM_" + processes + "SteamChild.exe";
+		unblock_connection(steam_process);
+	}
 }
-
-
-// Get the path to the FiveM.exe file
-std::string network::getFivemPath()
-{
-    // TODO: implement the logic to get the path to FiveM.exe
-    return "";
-}
-
-// Get the path to the subprocess directory
-std::string network::getSubprocessPath()
-{
-    // TODO: implement the logic to get the path to the subprocess directory
-    return "";
-}
-
-// Get the list of GTA versions
-std::vector<std::string> network::getGtaVersions()
-{
-    // TODO: implement the logic to get the list of GTA versions
-    return {};
-}
-
-// Unblock network connections to a file
-void network::unblockConnection(const std::string& filePath)
-{
-    // TODO: implement the logic to unblock network connections to a file
-}
-
